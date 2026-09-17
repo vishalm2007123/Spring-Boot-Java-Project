5b65a8f9ae930744ef29e3bee98a107356b7bffc
vishal.m2007123 <vishal.m2007123@gmail.com>
2026-09-17 09:14:46 +0530

    First day of spring boot camp

---
+++ JAVA EXECPT SESSION/firstapp/src/main/java/com/example/firstapp/Model/Controller/appcontroller.java
@@ -0,0 +1,4 @@
+package com.example.firstapp.Model.Controller;
+public class appcontroller {
+
+}
---
+++ JAVA EXECPT SESSION/firstapp/src/test/java/com/example/firstapp/FirstappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
